cf09dbe5a509f065712cfd8d794f9ba731e4e9f0
serhatbek <serhat.bekk@gmail.com>
2023-03-08 14:12:53 +0300

    object literals

--- 01-objects/1-object-literals/object-literals.js
+++ 01-objects/1-object-literals/object-literals.js
@@ -1,1 +1,14 @@
-console.log('object literals');
+const box = {
+  // object literals
+  radius: 1,
+  location: {
+    x: 1, // properties
+    y: 2, // properties
+  },
+  paint: () => {
+    // method
+    console.log('paint');
+  },
+};
+
+box.paint();
