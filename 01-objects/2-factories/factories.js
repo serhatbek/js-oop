b08b911a3f49f46533e71864188231e1c3a22a04
serhatbek <serhat.bekk@gmail.com>
2023-03-08 14:36:27 +0300

    constructor function

--- 01-objects/2-factories/factories.js
+++ 01-objects/2-factories/factories.js
@@ -15,4 +15,19 @@ box.paint();
 
 /**
  **if the key and the value are same, we can remove value (radius)
+ ** if you return an object, it's a factory object
+ ** if we use this keyword along with new operator, we refer to that function as a constructor function
  */
+
+// CONSTRUCTOR FUNCTION
+
+function Box(radius) {
+  this.radius = radius;
+  this.paint = () => {
+    console.log('constructor function, paint');
+  };
+}
+
+const box1 = new Box();
+
+box1.paint();
